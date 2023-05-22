738b7582224333225c1f9c4f7c29e6751117ff24
Midiã de Azevedo <70597509+Midia83@users.noreply.github.com>
2023-05-22 09:25:05 -0300

    Projeto Usuario

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -8,15 +8,3 @@ describe('template spec', () => {
     cy.visit('https://www.natura.com.br')
   })
 })
-
-
-const assert = require('assert');
-const {createUser} = require(".user");
-describe("Teste de cadastro de usuário",() => {
-  it("Deve retornar o usuário cadastrado com sucesso", () =>{
-    const user = createUser({
-name: ("")
-e-mail: ("")
-password: ("")
-
-    })
