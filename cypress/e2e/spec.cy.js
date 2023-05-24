068eae3c009490590f78eb1722a38a2f93eab62a
Midiã de Azevedo <70597509+Midia83@users.noreply.github.com>
2023-05-23 22:39:16 -0300

    Atualizar o spec.cy.js

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,15 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('home page', () => {
+  it('app deve estar on line', () => {
     cy.visit('https://www.aesop.com.br')
+
+describe('home page', () => {
+  it('app deve estar on line', () => {
+    cy.visit('https://www.aesop.com.br/login? redirect=%2F')
+
+describe('home page', () => {
+  it('app deve estar on line', () =>        cy.visit('https://www.aesop.com.br/login/?redirect=%2Fmeus-dados')
+
+
   
 
 describe('template spec', () => {
