9686df33e4b34056146ca8599cd1b906555bad2f
Midiã de Azevedo <70597509+Midia83@users.noreply.github.com>
2023-05-25 15:38:35 -0300

    Update cadastro_usuario.spec.js

--- e2e/cypress-integration/cadastro_usuario.spec.js
+++ e2e/cypress-integration/cadastro_usuario.spec.js
@@ -1,51 +1,39 @@
 describe('Cadastro de Usuário'), () => {
-    it('Cadastrar usuário com preenchimento de todos os campos'), () => {
-      cy.visit('https://www.aesop.com.br'); // Acessa o site Aesop Brasil
+  beforeEach(function() {
+      cy.visit('https://www.aesop.com.br') 
+    })
   
-      //Preenchimento dos campos de cadastro usuário
-
-      cy.get('Primeiro nome').type ('Luciana Helena');
-      cy.get('Sobrenome').type ('Silveira');
-      cy.get('E-mail').type ('luciana_helena_silveira@sdrifs.com.br');
-      cy.get('Senha').type ('Lu@silveira1010');
-      cy.get('Repita sua senha').type ('Lu@silveira1010');
-      cy.get('CPF').type ('192.975.139-72');
-      cy.get('Gênero').type ('feminino');
-      cy.get('Data de nascimento').type ('22/02/1958');
-      cy.get('Telefone').type ('(91) 98126-8696');
-      cy.get('btn-criar conta').click(); //Clica no botão criar conta
-  
-      //Verifica se o cadastro foi concluído com sucesso
-
+     it.only('preenche todos os campos de cadastro usuario ') 
+      cy.get('#firstName').type ('Luciana Helena');
+      cy.get('#lastName').type ('Silveira');
+      cy.get('#email').type ('luciana_helena_silveira@sdrifs.com.br');
+      cy.get('#senha').type ('Lu@silveira1010');
+      cy.get('#repita sua senha').type ('Lu@silveira1010');
+      cy.get('#cpf').type ('192.975.139-72');
+      cy.get('#gênero').type ('feminino');
+      cy.get('#data de nascimento').type ('22/02/1958');
+      cy.get('#phone').type ('(91) 98126-8696');
+      cy.get('button[type="criar conta"]').click();
       cy.url().should('eq', 'https://www.aesop.com.br/usuario/cadastro-sucesso');
       cy.get('mensagem-sucesso').should ('contain', 'usuário cadastrado com sucesso.');
 
+describe('Cadastro de Usuário'), () => {
+  beforeEach(function() {
+      cy.visit('https://wwww.natura.com.br')
+    })
 
-
-      //Cadastro usuário Natura Brasil
-
-      describe('Cadastro de Usuário'), () => {
-        it('Deve cadastrar usuário com preenchimento de todos os campos', () => {
-          cy.visit('https://wwww.natura.com.br'); //Acessa o site da Natura Brasil
-      
-          //Preenchimento dos campos de cadastro usuário
-
-          cy.get('Primeiro nome').type ('Marcelo Anthony');
-          cy.get('Sobrenome').type ('Sales');
-          cy.get('E-mail').type ('marceloanthonysales@debiasi.com.br');
-          cy.get('Senha').type ('Mar@sales2023');
-          CY.get('Repetir senha').type ('Mar@sales2023');
-          cy.get('CPF').type ('043.656.558-76');
-          cy.get('Data de nascimento').type ('26/01/1996');
-          cy.get('Gênero').type ('masculino');
-          cy.get('Celular').type ('(91) 98365-4776');
-
-    
-          cy.get('btn-criar conta').click(); //Clica no botão criar conta
-      
-          // Verifica se o cadastro foi concluído com sucesso
-
-          cy.url().should('eq', 'https://www.natura.com.br/usuario/cadastro-sucesso');
-          cy.get('mensagem-sucesso').should ('contain', 'usuário cadastrado com sucesso.');
-
-        });
+     it.only('preenche todos os campos de cadastro usuario ') 
+      cy.get('#firstName').type ('Marcelo Anthony');
+      cy.get('#lastName').type ('Sales');
+      cy.get('#email').type ('marceloanthonysales@debiasi.com.br');
+      cy.get('#senha').type ('Mar@sales2023');
+      cY.get('#repetir senha').type ('Mar@sales2023');
+      cy.get('#cpf').type ('043.656.558-76');
+      cy.get('#data de nascimento').type ('26/01/1996');
+      cy.get('#gênero').type ('masculino');
+      cy.get('#phone').type ('(91) 98365-4776');
+      cy.get('button[type="criar conta"]').click();
+      cy.url().should('eq', 'https://www.natura.com.br/usuario/cadastro-sucesso');
+      cy.get('mensagem-sucesso').should ('contain', 'usuário cadastrado com sucesso.');
+    }
+  }
